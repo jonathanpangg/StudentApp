9da933f2e9e4003f70c9d37ec684e67b5ee57e47
Jonathan Pang <66697656+pangj130@users.noreply.github.com>
2021-02-01 10:21:10 -0500

    p

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 var mongodb = require('mongodb')
 var assert = require('assert')
 var db = require('./mongoose')  
-const url = process.env.mongodb_URI || 'mongodb+srv://StudentUsers:Jonathan3388@cluster0.xbzay.mongodb.net/StudentApp?retryWrites=true&w=majority'
+const url = 'https://heroku-studentapp.herokuapp.com'
 var users = []
 app.use(express.json())
 
