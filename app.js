cf99436d86c478fa937219f41bd3afa76a35d32e
Jonathan Pang <66697656+pangj130@users.noreply.github.com>
2021-02-01 10:27:46 -0500

    p

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express()
 var mongodb = require('mongodb')
 var assert = require('assert')
 var db = require('./mongoose')  
-const url = 'https://heroku-studentapp.herokuapp.com'
+const url = process.env.mongodb_URI || 'mongodb+srv://StudentUsers:Jonathan3388@cluster0.xbzay.mongodb.net/StudentApp?retryWrites=true&w=majority'
 var users = []
 app.use(express.json())
 
 app.get('/users', (req, res) => {
     var resultArray = []
-    mongodb.connect(port, function (error, db) {
+    mongodb.connect(url, function (error, db) {
         if (error) throw error;
         var dbo = db.db('StudentApp')
         dbo.collection('Users').find({}).toArray(function(err, result) {
@@ -25,7 +25,7 @@ app.get('/users', (req, res) => {
 function getUsers() {
     var resultArray = []
     app.get('/users', (req, res) => {
-        mongodb.connect(port, function (error, db) {
+        mongodb.connect(url, function (error, db) {
             if (error) throw error;
             var dbo = db.db('StudentApp')
             dbo.collection('Users').find({}).toArray(function(err, result) {
@@ -42,7 +42,7 @@ function getUsers() {
 app.get('/users/:firstName/:lastName', (req, res) => {
     const first = req.params.firstName
     const last = req.params.lastName
-    mongodb.connect(port, function(error, db) {
+    mongodb.connect(url, function(error, db) {
         if (error) throw error
         var dbo = db.db('StudentApp')
         var query = { firstName: first, lastName: last }
@@ -56,7 +56,7 @@ app.get('/users/:firstName/:lastName', (req, res) => {
 })
 
 app.put('/users/:id/:date/:newDate', (req, res) => {
-    mongodb.connect(port, function(error, db) {
+    mongodb.connect(url, function(error, db) {
         if (error) throw error
         var dbo = db.db('StudentApp')
         var query = { date: req.params.date }
