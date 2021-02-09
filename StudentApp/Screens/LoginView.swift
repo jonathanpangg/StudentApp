c04dcbb7c2b62d9835fb8d18a1d15d38ec3a5e93
Jonathan Pang <66697656+pangj130@users.noreply.github.com>
2021-02-09 12:09:36 -0500

    organized files

---
+++ StudentApp/Screens/LoginView.swift
@@ -0,0 +1,199 @@
+//
+//  LoginView.swift
+//  StudentApp
+//
+//  Created by Jonathan Pang on 1/31/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @ObservedObject var pass: Pass
+    @ObservedObject var mode = ThemeStatus()
+    @ObservedObject var join = joinUser()
+    @State var users = [User]()
+    @State var username = ""
+    @State var password = ""
+    @State var secure = true
+    @State var alertStatus = false
+
+    func autoLog() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            if Double(Date().timeIntervalSince1970) - Double(join.user.date)! > 604800 {
+                pass.currentScreen = 5
+            }
+            else {
+                pass.currentScreen = 0
+            }
+        }
+    }
+    
+    func getBackground() -> Color {
+        if mode.mode.mode == "Default" {
+            return colorScheme != .dark ? Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)): Color.black
+        }
+        else if mode.mode.mode == "Light"{
+            return Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        }
+        else {
+            return Color.black
+        }
+    }
+    
+    func getForeground() -> Color {
+        if mode.mode.mode == "Default" {
+            return colorScheme != .dark ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)): Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        }
+        else if mode.mode.mode == "Light"{
+            return Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        }
+        else {
+            return Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        }
+    }
+    
+    func getUser() {
+        guard let url = URL(string: "http://localhost:1000/users") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            print(data)
+            if let decoded = try? JSONDecoder().decode([User].self, from: data) {
+                users = decoded
+            }
+        }.resume()
+    }
+    
+    func verifyUser() -> Bool {
+        for user in users {
+            if user.username == username && user.password == password {
+                return true
+            }
+        }
+        return false
+    }
+    
+    var body: some View {
+        ZStack {
+            getBackground()
+                .ignoresSafeArea(.all)
+            VStack {
+                ZStack {
+                    LoginTile(UIScreen.main.bounds.width / 8 * 7, UIScreen.main.bounds.height / 4, getBackground())
+                    VStack {
+                        HStack {
+                            Text("Username:")
+                                .foregroundColor(getForeground())
+                            Spacer()
+                        }
+                        .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(getForeground(), lineWidth: 1)
+                                 .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
+                            TextField("", text: $username)
+                                .autocapitalization(.none)
+                                .font(.system(size: 16))
+                                .foregroundColor(getForeground())
+                                .multilineTextAlignment(.leading)
+                                .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
+                        }
+                        
+                        HStack {
+                            Text("Password:")
+                                .foregroundColor(getForeground())
+                            Spacer()
+                        }
+                        
+                        .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(getForeground(), lineWidth: 1)
+                                .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
+                            HStack {
+                                if secure {
+                                    SecureField("", text: $password)
+                                        .autocapitalization(.none)
+                                        .font(.system(size: 16))
+                                        .foregroundColor(getForeground())
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: UIScreen.main.bounds.width / 64 * 33, height: UIScreen.main.bounds.height / 24)
+                                    Spacer()
+                                    Image(systemName: "eye.fill")
+                                        .foregroundColor(getForeground())
+                                        .onTapGesture {
+                                            withAnimation {
+                                                secure.toggle()
+                                            }
+                                        }
+                                }
+                                else {
+                                    TextField("Password: ", text: $password)
+                                        .autocapitalization(.none)
+                                        .font(.system(size: 16))
+                                        .foregroundColor(getForeground())
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: UIScreen.main.bounds.width / 16 * 9, height: UIScreen.main.bounds.height / 24)
+                                    Spacer()
+                                    Image(systemName: "eye.slash.fill")
+                                        .foregroundColor(getForeground())
+                                        .onTapGesture {
+                                            withAnimation {
+                                                secure.toggle()
+                                            }
+                                        }
+                                }
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
+                        }
+                    }
+                }
+                
+                HStack(alignment: .center) {
+                    ZStack {
+                        LoginTile(UIScreen.main.bounds.width / 2, UIScreen.main.bounds.height / 30, getBackground())
+                        Text("Register an account")
+                            .foregroundColor(getForeground())
+                    }
+                    .onTapGesture {
+                        pass.currentScreen = 5
+                    }
+                    Spacer()
+                    
+                    ZStack {
+                        LoginTile(UIScreen.main.bounds.width / 4, UIScreen.main.bounds.height / 30, getBackground())
+                        Text("Login")
+                            .foregroundColor(getForeground())
+                    }
+                    .onTapGesture {
+                        if verifyUser() {
+                            if let encoded = try? JSONEncoder().encode(join.user) {
+                                UserDefaults.standard.set(encoded, forKey: "saveUser")
+                            }
+                            pass.currentScreen = 0
+                        }
+                        else {
+                            alertStatus = true
+                        }
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width / 8 * 7)
+            }
+            .onAppear {
+                AppDelegate.orientationLock = UIInterfaceOrientationMask.portrait
+                UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+                UIViewController.attemptRotationToDeviceOrientation()
+            }
+            .onAppear(perform: getUser)
+            // .onAppear(perform: autoLog)
+            .alert(isPresented: $alertStatus) {
+                Alert(title: Text("Unable to Login"), message: Text("Either your username or password is incorrect"), dismissButton: .default(Text("Retry")))
+            }
+        }
+    }
+}
---
+++ StudentApp/Screens/MapView.swift
@@ -0,0 +1,62 @@
+//
+//  MapView.swift
+//  StudentApp
+//
+//  Created by Jonathan Pang on 1/26/21.
+//
+
+import SwiftUI
+import MapKit
+import CoreLocation
+
+struct MapView: UIViewRepresentable {
+    @ObservedObject var pass: Pass
+    @State var centerCoordinate = CLLocationCoordinate2D()
+    let locationManager = CLLocationManager()
+    
+    init(pass: Pass) {
+        self.pass = pass
+        setGeocodingData()
+    }
+    
+    func setGeocodingData() {
+        guard let url = URL(string: "http://ip-api.com/json") else { return }
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = [
+            "Content-Type": "application/json"
+        ]
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            if let decoded = try? JSONDecoder().decode(GeocodingData.self, from: data) {
+                DispatchQueue.main.async {
+                    pass.location = decoded.city
+                }
+            }
+        }.resume()
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        view.showsUserLocation = true
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            self.locationManager.startUpdatingLocation()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+                if let _ = self.locationManager.location {
+                    let locValue: CLLocationCoordinate2D = self.locationManager.location?.coordinate ?? CLLocationCoordinate2D()
+                    let coordinate = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+                    let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                    view.setRegion(region, animated: true)
+                }
+            })
+        }
+    }
+    
+}
