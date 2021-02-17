6963b551e915ab63e6660e3e3e3238706b6007d6
Jonathan Pang <66697656+pangj130@users.noreply.github.com>
2021-02-17 12:09:12 -0500

    p

---
+++ StudentApp/Helper/SettingData.swift
@@ -0,0 +1,20 @@
+//
+//  SettingData.swift
+//  StudentApp
+//
+//  Created by Jonathan Pang on 2/17/21.
+//
+
+import SwiftUI
+
+struct SettingData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingData_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingData()
+    }
+}
